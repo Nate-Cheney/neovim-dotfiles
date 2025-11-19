f9a8c00caf417677dd3cff12f62ab8a776d4b8ea
Nate Cheney <natecheney@proton.me>
2025-11-19 14:45:26 -0500

    Script to open github from a repo.

---
+++ nvim/bin/open-github.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Author: Nate Cheney
+# Filename: open-github.sh
+# Description: This script will open a git repository in github. 
+# Usage: ./open-github.sh
+# Options:
+#
+
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo "The current working dir is not tracked by git. $PWD"
+    exit 1
+
+else
+    origin=$(git remote get-url origin)
+
+    if [[ $origin =~ ^git@github.com:.+$ ]]; then
+        stripped1=${origin%.git}
+        stripped2=${stripped1##*:}
+        url="https://github.com/$stripped2"
+
+    elif [[ $origin =~ ^https:\/\/github.com\/.+$ ]]; then
+        url="${origin%.git}"
+
+    else 
+        echo "The remote origin is not github"
+        exit 1
+    fi
+
+    nohup xdg-open "$url" > /dev/null 2>&1 &
+    echo "Opened $url."
+fi
+
