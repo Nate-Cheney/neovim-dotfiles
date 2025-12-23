3fd61e14b9a758bf242c3f700900e5a53b35c380
Nate Cheney <natecheney@proton.me>
2025-12-22 20:02:22 -0500

    Attempting to fix initial packersync issues

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,15 +1,17 @@
 -- Set the colorscheme to catppuccin
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "macchiato"
 
--- Load the colorscheme
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
--- Make Window Transparent
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
-
+-- Only load if catppuccin is installed
+local ok, catppuccin = pcall(require, "catppuccin")
+if ok then
+  catppuccin.setup()
+  vim.cmd.colorscheme "catppuccin"
+  
+  -- Make Window Transparent
+  vim.cmd [[
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none
+    highlight NonText ctermbg=none
+  ]]
+end
