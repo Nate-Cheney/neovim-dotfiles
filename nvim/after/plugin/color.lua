3bad41aca85026c04ab65ab9dee66e798fb63233
Nate Cheney <natecheney@proton.me>
2025-12-27 10:36:19 -0500

    Fixed conflicting harpoon keybind

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,17 +1,15 @@
 -- Set the colorscheme to catppuccin
-vim.g.catppuccin_flavour = "macchiato"
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
--- Only load if catppuccin is installed
-local ok, catppuccin = pcall(require, "catppuccin")
-if ok then
-  catppuccin.setup()
-  vim.cmd.colorscheme "catppuccin"
-  
-  -- Make Window Transparent
-  vim.cmd [[
-    highlight Normal guibg=none
-    highlight NonText guibg=none
-    highlight Normal ctermbg=none
-    highlight NonText ctermbg=none
-  ]]
-end
+-- Load the colorscheme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+-- Make Window Transparent
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
