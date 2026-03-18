5e893d23ff22216c7df1e4803733b309e23c72f8
Nate Cheney <natecheney@proton.me>
2026-03-18 11:06:14 -0400

    Upload current config from nate-desktop

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,4 +1,9 @@
-require'nvim-treesitter.configs'.setup {
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+    return
+end
+
+ts.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "lua", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
