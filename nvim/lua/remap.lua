a0117e4428d66e5d7646368cdf34ec3e47e3e5a3
Nate Cheney <natecheney@proton.me>
2026-05-02 14:22:44 -0400

    'jj' to exit insert mode

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
+
+-- Normal mode
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>gh", "<cmd>!/$HOME/.config/nvim/bin/open-github.sh<CR>")
+vim.keymap.set("n", "<leader>gh", "<cmd>!/$HOME/.config/nvim/bin/open-github.sh<CR>", { desc = "Open the project in GitHub" })
+
+-- Insert mode
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
+
