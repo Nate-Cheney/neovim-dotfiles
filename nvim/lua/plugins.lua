03fec4fb99a58e515ccfe5e905c7d9f1ce16df18
Nate Cheney <natecheney@proton.me>
2026-05-30 11:04:53 -0400

    Change TS branch to master. Pin version.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -29,15 +29,18 @@ return require("packer").startup(function(use)
 	use { "catppuccin/nvim", as = "catppuccin" }
     
 	-- Telescope
-	use {
-		"nvim-telescope/telescope.nvim", 
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
+    use {
+        "nvim-telescope/telescope.nvim", 
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
    
     -- Treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         branch = 'main',
+        branch = 'master',  
+        commit = "7f53486",
         run = ':TSUpdate',
     }
 	
